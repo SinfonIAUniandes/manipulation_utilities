82c4579550d8e92ff6b255b22608790cdeba481a
Julián Andrés Méndez <s.tilde.eneca@gmail.com>
2023-07-03 11:54:40 -0500

    3 de jul 11:54

--- src/ManipulationPytoolkit.py
+++ src/ManipulationPytoolkit.py
@@ -4,7 +4,7 @@ from std_msgs.msg import String
 import ConsoleFormatter
 
 # Manipulation msgs
-from manipulation_msgs_pytoolkit.srv import GoToState, GoToAction, GraspObject
+from manipulation_msgs_pytoolkit.srv import GoToState, GoToAction, GoToActionRequest, GraspObject
 
 # Pytoolkit msgs
 from manipulation_msgs_pytoolkit.srv import set_angle_srv, set_angle_srvRequest
@@ -35,6 +35,7 @@ class ManipulationPytoolkit:
     def callbackGoToStatePytoolkit(self,req):
         request = set_angle_srvRequest()
         name = req.name
+        velocity = req.velocity
         angle = []
         joints_arms = ["LShoulderPitch", "LShoulderRoll", "LElbowYaw", "LElbowRoll", "LWristYaw", "RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll", "RWristYaw"]
         joints_head = ["HeadPitch", "HeadYaw"]
@@ -53,6 +54,8 @@ class ManipulationPytoolkit:
             request.name = joint_right_hand 
         elif name == "open_both_hands" or name == "close_both_hands":  
             request.name = joint_hands
+        else: 
+            return "State not found"
 
         # Arms
         if(name=="box"):
@@ -96,18 +99,18 @@ class ManipulationPytoolkit:
         elif(name == "close_both_hands"):
             angle = [0.0, 0.0]
 
-        try:
-            request.angle = angle
-            request.speed = 0.1
-            res = self.motionSetAngleClient.call(request)
-            return res.result
-        except rospy.ServiceException as exc:
-            print("Service did not process request: " + str(exc))
+        request.angle = angle
+        request.speed = velocity
+        res = self.motionSetAngleClient.call(request)
+        return res.result
 
     ###################################################### Go to action ######################################################
 
-    def goToActionPytoolkit(self, name):
+    def callbackGoToActionPytoolkit(self, req):
+        request = set_angle_srvRequest()
+        name = req.name
         angle = []
+
         joints_arms_hands = ["LShoulderPitch", "LShoulderRoll", "LElbowYaw", "LElbowRoll", "LWristYaw", "RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll", "RWristYaw", "LHand", "RHand"]
         joints_arms = ["LShoulderPitch", "LShoulderRoll", "LElbowYaw", "LElbowRoll", "LWristYaw", "RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll", "RWristYaw"]
         joints_hip = ["HipPitch"]
@@ -115,152 +118,167 @@ class ManipulationPytoolkit:
 
         if(name=="place_both_arms"):
             # Se agacha
-            angle_1 = [-0.3_5]
-            setAction_1 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)  
+            angle_1 = [-0.35]
+
+            # Se agacha
+             = [0.0, 0.0]
 
             # Separa los brazos
-            angle_2 = [0.00383162, 3.8160, -0.00378999, -0.00877543, -4.09046e-05, -9.91789e-05, -3.8160, -9.95662e-05, 0.00881285, 7.63354e-05, 0-5, 0.5]
-            setAction_2 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
+            angle_3 = [0.00383162, 3.8160, -0.00378999, -0.00877543, -4.09046e-05, -9.91789e-05, -3.8160, -9.95662e-05, 0.00881285, 7.63354e-05, 0-5, 0.5]
 
             # giras los brazos
-            angle_3 = [1.55179, 1.56201, 0.0039061, -0.00881861, 0.00328999, 1.57469, -1.56204, 1.21692e-05, 0.00882497, -9.82796e-05, 0-5, 0.5]
-            setAction_3 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
+            angle_4 = [1.55179, 1.56201, 0.0039061, -0.00881861, 0.00328999, 1.57469, -1.56204, 1.21692e-05, 0.00882497, -9.82796e-05, 0-5, 0.5]
 
             # Bajas los brazos
-            angle_4 = [1.57471, 0.0940139, 0.00371943, -0.00879696, 0.00338298, 1.57466, -0.0797469, 2.59231e-05, 0.0087464, 3.54903e-05, 0-5, 0.5]
-            setAction_4 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
+            angle_5 = [1.57471, 0.0940139, 0.00371943, -0.00879696, 0.00338298, 1.57466, -0.0797469, 2.59231e-05, 0.0087464, 3.54903e-05, 0-5, 0.5]
 
-            try:
-                action_1 = setAction_1(joints_hip , angle_1, 0.2)
-                rospy.sleep(2)
-                action_2 = setAction_2(joints_arms_hands, angle_2, 0.1)
-                rospy.sleep(2)
-                action_3 = setAction_3(joints_arms_hands, angle_3, 0.25)
-                rospy.sleep(0.5)
-                action_4 = setAction_4(joints_arms_hands, angle_4, 0.15)
-                rospy.sleep(2)
-                return "place both arms executed"
+            request.name = joints_hip
+            request.angle = angle_1
+            request.speed = 0.2
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(2)
 
-            except rospy.ServiceException as exc:
-                print("Service did not process request: " + str(exc))
+            request.name = joints_arms_hands
+
+            request.name = joint_hands
+            request.angle = angle_2
+            request.speed = 0.2
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(2)
+
+            request.angle = angle_3
+            request.speed = 0.1
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(2)
+
+            request.angle = angle_3
+            request.speed = 0.25
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(0.5)
+
+            request.angle = angle_4 
+            request.speed = 0.15
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(2)
+            return "place both arms executed"
         
         elif(name=="place_left_arm"):
             # Giras brazo
             angle_1 = [0.00383162, 3.8160, -0.00378999, -0.00877543, -4.09046e-05, -9.91789e-05, -3.8160, -9.95662e-05, 0.00881285, 7.63354e-05, 1.0, 0.5]
-            setAction_1 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
 
             # Bajas brazo
             angle_2 = [0.796827, 0.00881551, -1.39942, -0.525606, 1.39701, 1.5746, -0.00885075, 1.56701, 0.00882722, 0.230255, 1, 0.5]
-            setAction_2 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
             
             # Abrir mano
             angle_3 = [0.0, 0.5]
-            setAction_3 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
 
             # Girar brazo
             angle_4 = [0.796941, 1.56198, -1.39166, -0.00880171, 1.3969, 1.57448, -0.0089395, 1.56695, 0.00884955, 0.230282]
-            setAction_4 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
 
             # Girar brazo
             angle_5 = [1.56707, 1.56203, -1.39928, -0.00880251, 1.39715, 1.57455, -0.00897639, 1.56704, 0.00898821, 0.230243]
-            setAction_5 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
 
             # Bajar brazo
-            angle_5 = [1.56708, 1.56708, 1.56708, -0.00876288, -0.230052, -0.230052, -0.00896259, 1.56709, 0.00902427, 0.230202]
-            setAction_5 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
+            angle_6 = [1.56708, 1.56708, 1.56708, -0.00876288, -0.230052, -0.230052, -0.00896259, 1.56709, 0.00902427, 0.230202]
 
-            try:
-                action_1 = setAction_1(joints_arms_hands , angle_1, 0.2)
-                rospy.sleep(2)
-                action_2 = setAction_2(joints_arms_hands , angle_2, 0.2)
-                rospy.sleep(2)
-                action_3 = setAction_2(joint_hands , angle_3, 0.2)
-                rospy.sleep(1)
-                action_4 = setAction_2(joints_arms , angle_4, 0.2)
-                rospy.sleep(2)
-                action_5 = setAction_2(joints_arms , angle_5, 0.2)
-                rospy.sleep(1.5)
-                action_6 = setAction_6(joints_arms , angle_6, 0.2)
-                return "place left arm executed"
-            
-            except rospy.ServiceException as exc:
-                print("Service did not process request: " + str(exc))
+            request.name = joints_arms_hands
+            request.angle = angle_1
+            request.speed = 0.2
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(2)
+
+            request.angle = angle_2
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(2)
+
+            request.name = joint_hands
+            request.angle = angle_3
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(1)
+
+            request.name = joints_arms
+            request.angle = angle_4
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(2)
+
+            request.angle = angle_5
+            res = self.motionSetAngleClient.call(request)
+
+            request.angle = angle_6
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(1.5)
+
+            return "place left arm executed"
 
         elif(name=="place_right_arm"):
             # Giras brazo
             angle_1 = [1.56705, 0.0087755, -1.57479, -0.00876018, -0.00327626, 0.529962, -0.00886489, 1.39943, 0.531231, -1397, 0.5, 1.0]
-            setAction_1 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
 
             # Bajas brazo
             angle_2 = [1.5747, 0.00874223, -1.57477, -0.00876009, -0.00328602, 0.781676, -0.00882641, 1.39931, 0.531273, -1.39709, 0.5, 1.0]
-            setAction_2 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
             
             # Abrir mano
             angle_3 = [0.5, 0.0]
-            setAction_3 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
 
             # Extender brazo
             angle_4 = [1.57482, 0.00877891, -1.57479, -0.00882567, -0.00329107, 0.781602, -1.56204, 1.57468, 0.00876204, -1.39709]
-            setAction_4 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
 
             # Girar brazo
             angle_5 = [1.57483, 0.00886465, -1.57476, -0.00883808, -0.00334844, 1.56717, -1.56198, 1.57451, 0.00885734, -1.39706]
-            setAction_5 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
 
             # Bajar brazo
             angle_6 = [1.57482, 0.00886047, -1.57472, -0.00891162, -0.00343008, 1.56711, -0.00879193, 2.08561, 0.00880696, -0.523587]
-            setAction_6 = rospy.ServiceProxy('pytoolkit/ALMotion/set_angle_srv', set_angle_srv)
 
-            try:
-                action_1 = setAction_1(joints_arms_hands , angle_1, 0.2)
-                rospy.sleep(2)
-                action_2 = setAction_2(joints_arms_hands , angle_2, 0.2)
-                rospy.sleep(2)
-                action_3 = setAction_3(joint_hands , angle_3, 0.2)
-                rospy.sleep(1)
-                action_4 = setAction_4(joints_arms , angle_4, 0.1)
-                rospy.sleep(2)
-                action_5 = setAction_5(joints_arms , angle_5, 0.2)
-                rospy.sleep(1.5)
-                action_6 = setAction_6(joints_arms , angle_6, 0.2)
-                return "place right arm executed"
+            request.name = joints_arms_hands
+            request.angle = angle_1
+            request.speed = 0.2
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(2)
 
-            except rospy.ServiceException as exc:
-                print("Service did not process request: " + str(exc))
+            request.angle = angle_2
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(2)
 
-    def callbackGoToActionPytoolkit(self, req):
-        #Args: name of the position
-        res = self.goToActionPytoolkit(req.name)
-        return res
+            request.name = joint_hands
+            request.angle = angle_3
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(1)
+
+            request.name = joints_arms
+            request.angle = angle_4
+            request.speed = 0.1
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(2)
+
+            request.angle = angle_5
+            request.speed = 0.2
+            res = self.motionSetAngleClient.call(request)
+
+            request.angle = angle_6
+            request.speed = 0.2
+            res = self.motionSetAngleClient.call(request)
+            rospy.sleep(1.5)
+            return "place right arm executed"
+
 
     ###################################################### Grasp object ######################################################
 
-    def graspObjectPytoolkit(self, object):
-        list_carry_right_arm_1 = ["fork", "spoon", "knife", "mug", "bottle", "cereal_box", "milk"]
-        list_hold_both_arms_3 = ["bowl", "plate" ]
-
-        if(object in list_carry_right_arm_1):
-            carry_right_arm_1 = rospy.ServiceProxy('pytoolkit/ALMotion/goToStatePytoolkit', GoToStatePytoolkit)
-            try:
-                object = carry_right_arm_1("carry_right_arm_1")
-                return "carry right arm executed"
-
-            except rospy.ServiceException as exc:
-                print("Service did not process request: " + str(exc))
-        
-        elif(object in list_hold_both_arms_3):
-            hold_both_arms_3 = rospy.ServiceProxy('pytoolkit/ALMotion/goToStatePytoolkit', GoToStatePytoolkit)
-            try:
-                object = hold_both_arms_3("hold_both_arms_3")
-                return "hold both arms executed"
-
-            except rospy.ServiceException as exc:
-                print("Service did not process request: " + str(exc))
-
     def callbackGraspObjectPytoolkit(self, req):
-        #Args: name of the position
-        res = self.graspObjectPytoolkit(req.object)
-        return res
+        request = GoToActionRequest()
+
+        list_1 = ["fork", "spoon", "knife", "mug", "bottle", "cereal_box", "milk"]
+        list_2 = ["bowl", "plate" ]
+        name_object = req.name 
+        
+        if(name_object in list_1):
+            request.name = "small_object_left_hand"
+            res = self.motionSetAngleClient.call(request)
+            return "Pose executed"
+
+        elif(name_object in list_2):
+            request.name = "bowl"
+            res = self.motionSetAngleClient.call(request)
+            return "Pose executed"
 
 if __name__ == '__main__':
     consoleFormatter=ConsoleFormatter.ConsoleFormatter()
